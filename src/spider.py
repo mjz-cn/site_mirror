d6521b1da9f68a13ad498e4ebbe6008a46a820f8
majunzhe <majunzhe@didichuxing.com>
2018-07-29 23:52:27 +0800

    init

---
+++ src/spider.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+
+class Spider:
+    """
+        组合其它组件来完成下载任务
+    """
+
+    def __init__(self):
+        scheduler = ''
+        pipeline = None
+        downloader = None
+        processor = None
+        pass
+
+    def run(self):
+        pass
+        # 检测当前运行状态
+        # 初始化各个组件
+        # 初始化请求队列
+        # 开始运行
+
