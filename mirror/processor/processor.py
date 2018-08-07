13b90228e9428f3c9af4ac3cab64383ab111e8d2
majunzhe <majunzhe@didichuxing.com>
2018-08-08 00:36:06 +0800

    init

--- mirror/processor/processor.py
+++ mirror/processor/processor.py
@@ -4,26 +4,29 @@ from urllib.parse import urljoin
 
 from pyquery import PyQuery as pq
 
+from mirror.libs.components import Request
+
+from mirror.libs import tools
+
 
 class PageProcessor:
 
     def __init__(self, task):
         self.task = task
+        self.task_domain = tools.get_domain(task.get_site().domain)
         self.task_key = self.task.get_uuid()
 
-    def _abs_url(self, origin_url, url):
+    def get_target_request(self, page, target_url):
         """
         重新构造url，过滤掉无效的字符
-        :param origin_url:
-        :param url:
-        :return:
         """
-        if url.startswith('#'):
+        if target_url.startswith('#') or 'javascript:;' == target_url:
             return
-        target_url = urljoin(origin_url, url)
-        if '#' in target_url:
-            pos = target_url.find('#')
-        return target_url
+        target_url = urljoin(page.url, target_url)
+        # 检测url域名是否与当前网站域名一致
+        if not tools.has_same_domain(self.task_domain, target_url):
+            return
+        return Request(target_url)
 
     def process(self, page):
         """
@@ -39,11 +42,7 @@ class PageProcessor:
             target_url = tag.get('href')
             if not target_url:
                 target_url = tag.get('src')
-            # 生成绝对的url
-            target_url = self._abs_url(page.url, target_url)
-
-        # 提取所有的url
-
-        # 组合成合适的url
-
-        # 过滤掉不属于当前网站的url
+            # 生成目标request
+            target_request = self.get_target_request(page, target_url)
+            if target_request:
+                page.add_target_request(target_request)
