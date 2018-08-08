de2d6c3e66a08f1c8f1a62e6026ff1fb6190b38a
majunzhe <majunzhe@didichuxing.com>
2018-08-08 17:11:46 +0800

    init

---
+++ bin/init.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+CURRDIR=`dirname "$0"`
+# 当前目录
+BASEDIR=`cd ${CURRDIR}; pwd`
+
+PROJECT_DIR=`cd ${BASEDIR}/..;pwd`
+CONFIG_DIR=${PROJECT_DIR}/config
+
+
+echo "------------------------- 开始初始化python虚拟环境 -------------------------"
+# 初始化python环境
+cd ${PROJECT_DIR}
+# 创建虚拟环境
+python3 -m venv venv
+# 激活venv
+source 'venv/bin/activate'
+# 安装python3依赖库
+${PROJECT_DIR}/venv/bin/pip3 install -r requirements.txt
+echo "------------------------- 成功初始化python虚拟环境 -------------------------"
+
+
+echo -e '\n\n\n'
+
+echo "------------------------- 开始初始化mysql数据库 -------------------------"
+# 初始化数据库
+cd ${CONFIG_DIR}
+host=`awk -F '=' '{if (! ($0 ~ /^;/) && $0 ~ /host/) print $2}' conf.ini`
+user=`awk -F '=' '{if (! ($0 ~ /^;/) && $0 ~ /user/) print $2}' conf.ini`
+password=`awk -F '=' '{if (! ($0 ~ /^;/) && $0 ~ /password/) print $2}' conf.ini`
+mysql -h ${host} -u ${user} -p"${password}" < mirror.sql
+echo "------------------------- 成功初始化mysql数据库 -------------------------"
---
+++ config/mirror.sql
@@ -0,0 +1,5 @@
+
+-- 创建database
+CREATE DATABASE IF NOT EXISTS mirror CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
